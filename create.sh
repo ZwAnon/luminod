0f4b6fd34e6b622cac05620f2b44dca78b0bd92f
TJ Phippen <tj@tjphippen.com>
2021-04-23 16:18:28 -0500

    submod & update create.sh

--- create.sh
+++ create.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 service=$1
-wwwdir="images/php/app/"
+wwwdir="/var/www/html"
 
 cp .env.docker.example .env
+cp .env.app.example images/php/app/.env
 
 docker-compose up --build -d
 
-git clone https://github.com/laravel/lumen.git $wwwdir
-cp .env.app.example images/php/app/.env
-
-#docker-compose run --rm -w /var/www/html/app php composer install
+docker-compose run --rm -w $wwwdir/app php composer install
