8010694230167e763bc39f5255e3758017fa2dde
TJ Phippen <tj@tjphippen.com>
2021-04-23 16:59:02 -0500

    working submodule

--- create.sh
+++ create.sh
@@ -3,6 +3,9 @@
 service=$1
 wwwdir="/var/www/html"
 
+git submodule add https://github.com/laravel/lumen images/php/app
+git submodule update
+
 cp .env.docker.example .env
 cp .env.app.example images/php/app/.env
 
