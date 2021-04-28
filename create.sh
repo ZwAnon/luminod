ad133dcf2531220cb5805b73b322b97d0be88f01
TJ Phippen <tj@tjphippen.com>
2021-04-28 11:33:58 -0500

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,6 +3,8 @@
 service=$1
 wwwdir="/var/www/html"
 
+# sudo apt update -y
+
 git submodule add https://github.com/laravel/lumen images/php/app
 git submodule update
 
@@ -12,3 +14,5 @@ cp .env.app.example images/php/app/.env
 docker-compose up --build -d
 
 docker-compose run --rm -w $wwwdir/app php composer install
+
+# todo: adduser $1_user && usermod -aG sudo $1_user && su - $1_user
